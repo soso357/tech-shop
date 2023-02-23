90d81fc5c2d209d854696441abaa74101dac6ab9
iosebi <nebieridze123456789@gmail.com>
2023-02-23 17:14:00 +0400

    first commit

---
+++ src/pages/shop/product.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from "react";
+import { ShopContext } from "../../context/shop-context";
+export const Product = (props) => {
+  const { id, productName, price, productImage } = props.data;
+
+  const { addToCart, cartItems } = useContext(ShopContext);
+
+  const cartItemsAmount = cartItems[id];
+
+  return (
+    <div className="product">
+      <img src={productImage} />
+      <div className="deescription">
+        <p>
+          <b> {productName} </b>
+        </p>
+        <p>${price}</p>
+      </div>
+      <button className="addToCartBttn" onClick={() => addToCart(id)}>
+        Add To Cart {cartItemsAmount > 0 && <>({cartItemsAmount})</>}
+      </button>
+    </div>
+  );
+};
---
+++ src/products.js
@@ -0,0 +1,45 @@
+import product1 from './assets/1.jpg';
+import product2 from './assets/2.jpg';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+import product5 from './assets/5.jpg';
+import product6 from './assets/6.jpg'
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        produuctName: "iphone",
+        price: 2000,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        produuctName: "iphone",
+        price: 1100,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        produuctName: "iphone",
+        price: 800,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        produuctName: "iphone",
+        price: 350,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        produuctName: "iphone",
+        price: 900,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        produuctName: "iphone",
+        price: 250,
+        productImage: product6,
+    }
+]
